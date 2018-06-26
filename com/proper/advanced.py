5cf5b5ac0d15936f371a59a0a751519e7c04cc3f
王泽洲 <death4note@yeah.net>
2018-06-26 15:00:20 +0800

    高级特性，补全生成器

--- com/proper/advanced.py
+++ com/proper/advanced.py
@@ -1,6 +1,7 @@
 # 高级特性
 # 通过 collections 模块的 Iterable 类型判断当前模块是否可以被迭代
 from collections import Iterable
+from collections import Iterator
 
 
 def slices_demo():
@@ -75,4 +76,81 @@ def list_comprehensions_demo():
     print(l_list)
 
 
-list_comprehensions_demo()
+def generator_demo():
+    # 通过列表生成式创建大容量的列表，所需内存太大，
+    # 如果列表元素可以按照某种算法推算出来，
+    # 那我们是否可以在循环的过程中不断推算出后续的元素呢？
+    # 这样就不必创建完整的 list，从而节省大量的空间。
+    # 这种一边循环一边计算的机制，称为生成器：generator
+    # 要创建一个 generator，有很多种方法。
+    # 第一种方法很简单，只要把一个列表生成式的[]改成()
+
+    g = (x * x for x in range(1, 10))
+    print(g)
+    # g 是一个 generator 可以通过 next() 打印出 g 的下一个元素
+    print(next(g))
+
+    for x in g:
+        print(x)
+
+    print(fib(8))
+
+    # 实际上 generator 是从这一段开始执行
+    for y in fib(10):
+        print(y)
+
+    for y in triangle(10):
+        print(y)
+    s_1, s_0 = [1], [0]
+
+    print((s_1 + s_0)[0])
+
+
+# generator 和函数的执行流程不一样。
+# 函数是顺序执行，遇到return语句或者最后一行函数语句就返回。
+# 而变成 generator 的函数，在每次调用 next() 的时候执行，(外部调用next())
+# 遇到 yield 语句返回，再次执行时从上次返回的 yield 语句处继续执行。
+def fib(max_num):
+    n, a, b = 0, 0, 1
+    while n < max_num:
+        # 如果一个函数定义中包含 yield 关键字，那
+        # 么这个函数就不再是一个普通函数，而是一个 generator
+
+        # 首次执行到这个地方发生中断
+        yield b
+        # 之后接着向↓接着执行
+        a, b = b, a+b
+        n = n+1
+
+        # 但是用for循环调用generator时，发现拿不到generator的return语句的返回值。
+        # 如果想要拿到返回值，必须捕获StopIteration错误，返回值包含在StopIteration的value中
+    return 'done'
+
+
+def triangle(max_length):
+    a, L = 0, [1]
+    while a < max_length:
+        yield L
+        L = [(L+[0])[i] + ([0]+L)[i] for i in range(len(L) + 1)]
+        a = a + 1
+
+
+def iterable_demo():
+    # 而生成器不但可以作用于 for 循环，还可以被 next() 函数不断调用并返回下一个值，
+    # 直到最后抛出 StopIteration 错误表示无法继续返回下一个值了。
+    # 可以被 next() 函数调用并不断返回下一个值的对象称为迭代器: Iterator。
+    # 可以使用 isinstance() 判断一个对象是否是 Iterator 对象
+
+    print(isinstance((x for x in range(1, 4)), Iterator))
+    print(isinstance([], Iterator))
+    print(isinstance({}, Iterator))
+    print(isinstance('abc', Iterator))
+
+    # 生成器都是 Iterator 对象，但 list、dict、str 虽然是 Iterable，却不是 Iterator
+    # 把 list、dict、str 等 Iterable 变成 Iterator 可以使用 iter() 函数
+
+    print(isinstance(iter([]), Iterator))
+    print(isinstance(iter('abc'), Iterator))
+
+
+generator_demo()
